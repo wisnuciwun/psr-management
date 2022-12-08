a49e0b13c286d5106245da6c29ef22cadb46f747
wisnuciwun <adiwardhanawisnu@gmail.com>
2022-12-08 23:58:13 +0700

    feat: crowd control

---
+++ src/constants/tempStructure.js
@@ -0,0 +1,101 @@
+module.exports = {
+     data_member: [
+          {
+               name: 'Wahyu Saputra',
+               address: 'A1 No. 11',
+               position: 'Ketua RT',
+               img: 'https://assets.promediateknologi.com/crop/0x0:717x674/x/photo/2022/11/03/674327351.jpeg'
+          },
+          {
+               name: 'Gugun',
+               address: 'I2 No. 09',
+               position: 'Wakil Ketua RT',
+               img: 'https://i.ibb.co/XXSgwBS/Ugun-Merdekawati.jpg'
+          },
+          {
+               name: 'Nino',
+               address: 'J2-18',
+               position: 'Sekretaris',
+               img: 'https://i.ibb.co/C6gRpND/Nino.jpg'
+          },
+          {
+               name: 'Andi Setiandi',
+               address: 'B3-02',
+               position: 'Bendahara',
+               img: 'https://i.ibb.co/G0zPX2f/Andi-Setiandi.jpg'
+          },
+          {
+               name: ' Asep Gunadi',
+               address: 'B3-04',
+               position: 'Koordinator Blok 1 (A-B)',
+               img: 'https://i.ibb.co/JnCwpQL/si-Hawhaw.jpg'
+
+          },
+          {
+               name: 'Gunawan Seto',
+               address: 'C4-04',
+               position: 'Koordinator Blok 2 (C-D)',
+               img: ''
+          },
+          {
+               name: 'Lucky',
+               address: 'E2-99',
+               position: 'Koordinator Blok 3 (E-F)',
+               img: 'https://i.ibb.co/pwPgkVj/Lucky-Nugraha.jpg'
+          },
+          {
+               name: 'Rangga',
+               address: 'G',
+               position: 'Koordinator Blok 5 (I-J)',
+               img: 'https://i.ibb.co/82M7KWM/Rangga-Darmajati.jpg'
+          },
+          {
+               name: 'Ahmad',
+               address: 'C3-10',
+               position: 'Fungsional (Badan Kelengkapan) Hubungan Masyarakat',
+               img: 'https://i.ibb.co/jfFKBL0/Ahmad-Rahmadi.jpg'
+          },
+          {
+               name: 'Nona',
+               address: 'C1-11',
+               position: 'Fungsional (Badan Kelengkapan) Hubungan Masyarakat',
+               img: 'https://i.ibb.co/DMqdBwt/Nona-Anandha-Diningrat.jpg'
+          },
+          {
+               name: 'Sigit',
+               address: 'H1-18',
+               position: 'Perencanaan dan Pengembangan Masyarakat',
+               img: 'https://i.ibb.co/jTCLrkd/SIGIT-BAIHAQI.jpg'
+          },
+          {
+               name: 'Nendar',
+               address: 'I4-05',
+               position: 'Perencanaan dan Pengembangan Masyarakat',
+               img: ''
+          },
+          {
+               name: 'Rimba',
+               address: 'B4-10',
+               position: 'Keamanan, Ketertiban dan Kebersihan',
+               img: 'https://i.ibb.co/M6Qqxg0/Rimba.jpg'
+          },
+          {
+               name: 'Yudi',
+               address: 'E2',
+               position: 'Keamanan, Ketertiban dan Kebersihan',
+               img: 'https://i.ibb.co/M6Qqxg0/Rimba.jpg'
+          },
+          {
+               name: 'Fahzis',
+               address: 'D2',
+               position: 'Media dan Teknologi Informasi',
+               img: 'https://i.ibb.co/MVdBzdQ/Mohamad-Fahrul-Azis.jpg'
+          },
+          {
+               name: 'Wisnu',
+               address: 'B5-21',
+               position: 'Media dan Teknologi Informasi',
+               img: 'https://i.ibb.co/ssc6qPZ/Wisnu-Wardhana.jpg'
+          },
+     ]
+}
--- src/pages/OrganizationStructure.js
+++ src/pages/OrganizationStructure.js
@@ -1,30 +1,26 @@
+import { data_member } from 'constants/tempStructure'
 import React, { Component, Fragment } from 'react'
+import { Card, CardText } from 'reactstrap'
 
 export class OrganizationStructure extends Component {
      render() {
           return (
                <Fragment>
-                    <div className='w-100 p-3'>
-                        <p>Ketua: Wahyu Saputra (A1-11)</p>
-                        <p>Wakil Ketua: Gugun (I2-09)</p>
-                        <p>Sekretaris: Nino (J2-18)</p>
-                        <p>Bendahara: Andi Setiandi (B3-02)</p>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                         
-                        
-                         
-                         {/* Struktural
-                         Koordinator Blok 1 (A-B)		: Asep Gunadi (B3-04)
-                         Koordinator Blok 2 (C-D)		: Gunawan Seto (C4-04)
-                         Koordinator Blok 3 (E-F)		: Lucky (E2
-                         Koordinator Blok 4 (G-H)		: Rangga (G
-                         Koordinator Blok 5 (I-J)		: Reno (I3-11) */}
-
+                    <div className='w-100 p-3 d-flex flex-wrap'>
+                         {
+                              data_member.map((v, id) => {
+                                   return (
+                                        <Card style={{width: id === 0 ? '100%' : '42%', margin: '10px'}} body className=''>
+                                             <CardText className='font-xl'>{v.name}</CardText>
+                                             <img style={{height: '200px', objectFit: 'cover'}} src={v.img} alt="" />
+                                             <CardText className='mt-2'>
+                                                  <div className='font-md'>{v.position}</div>
+                                                  <div className='font-md font-weight-bold'>{v.address}</div>
+                                             </CardText>
+                                        </Card>
+                                   )
+                              })
+                         }
                     </div>
                </Fragment>
           )
